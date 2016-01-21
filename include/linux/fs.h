f6de13f9fe47c47e679ade4e4cbedf0cf479a639
Seth Forshee <seth.forshee@canonical.com>
2016-01-21 15:37:53 -0600

    UBUNTU: SAUCE: overlayfs: Propogate nosuid from lower and upper mounts
    An overlayfs mount using an upper or lower directory from a
    nosuid filesystem bypasses this restriction. Change this so
    that if any lower or upper directory is nosuid at mount time the
    overlayfs superblock is marked nosuid. This requires some
    additions at the vfs level since nosuid currently only applies to
    mounts, so a SB_I_NOSUID flag is added along with a helper
    function to check a path for nosuid in both the mount and the
    superblock.
    
    BugLink: http://bugs.launchpad.net/bugs/1534961
    BugLink: http://bugs.launchpad.net/bugs/1535150
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    [saf: Fix up conflicts]
    
    Conflicts:
    	fs/exec.c
    	security/commoncap.c
    	security/selinux/hooks.c

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1299,6 +1299,7 @@ struct mm_struct;
 /* sb->s_iflags */
 #define SB_I_CGROUPWB	0x00000001	/* cgroup-aware writeback enabled */
 #define SB_I_NOEXEC	0x00000002	/* Ignore executables on this fs */
+#define SB_I_NOSUID	0x00000004	/* Ignore suid on this fs */
 
 /* Possible states of 'frozen' field */
 enum {
@@ -3118,5 +3119,6 @@ static inline bool dir_relax(struct inode *inode)
 
 extern bool path_noexec(const struct path *path);
 extern void inode_nohighmem(struct inode *inode);
+extern bool path_nosuid(const struct path *path);
 
 #endif /* _LINUX_FS_H */
