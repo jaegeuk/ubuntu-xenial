smvoss/ubuntu-precise

UBUNTU: SAUCE: overlayfs: when copying up and reading directories ensure mounter had permissions V2
When copying up files and reading directory contents ensure the mounter has
permissions for the operation over the consituent parts (lower and upper).
Where we are in a namespace this ensures that the mounter (root in that
namespace) has permissions over the files and directories, preventing
exposure of protected files and directory contents.

CVE-2015-1328
Signed-off-by: Andy Whitcroft <apw@canonical.com>
Acked-by: Tyler Hicks <tyhicks@canonical.com>
Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

fs/overlayfs/readdir.c
@@ -14,6 +14,7 @@
 #include <linux/xattr.h>
 #include <linux/rbtree.h>
 #include <linux/security.h>
+#include <linux/sched.h>
 #include "overlayfs.h"
 
 struct ovl_cache_entry {
@@ -302,6 +303,11 @@ static int ovl_readdir(struct file *file, void *buf, filldir_t filler)
 		ovl_dir_reset(file);
 
 	if (od->is_real) {
+		res = ovl_dentry_root_may(file->f_path.dentry,
+					  &(od->realfile->f_path), MAY_READ);
+		if (res)
+			return res;
+
 		res = vfs_readdir(od->realfile, filler, buf);
 		file->f_pos = od->realfile->f_pos;
 
@@ -315,6 +321,13 @@ static int ovl_readdir(struct file *file, void *buf, filldir_t filler)
 
 		ovl_path_lower(file->f_path.dentry, &lowerpath);
 		ovl_path_upper(file->f_path.dentry, &upperpath);
+
+		res = ovl_dentry_root_may(file->f_path.dentry, &upperpath, MAY_READ);
+		if (res)
+			return res;
+		res = ovl_dentry_root_may(file->f_path.dentry, &lowerpath, MAY_READ);
+		if (res)
+			return res;
 
 		res = ovl_dir_read_merged(&upperpath, &lowerpath, &rdd);
 		if (res) {
@@ -465,6 +478,13 @@ static int ovl_check_empty_dir(struct dentry *dentry, struct list_head *list)
 
 	ovl_path_upper(dentry, &upperpath);
 	ovl_path_lower(dentry, &lowerpath);
+
+	err = ovl_dentry_root_may(dentry, &upperpath, MAY_READ);
+	if (err)
+		return err;
+	err = ovl_dentry_root_may(dentry, &lowerpath, MAY_READ);
+	if (err)
+		return err;
 
 	err = ovl_dir_read_merged(&upperpath, &lowerpath, &rdd);
 	if (err)

jaegeuk/ubuntu-xenial

Acked-by: Serge E. Hallyn <serge.hallyn@ubuntu.com>
Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
 Conflicts:
	fs/overlayfs/copy_up.c

fs/overlayfs/readdir.c
@@ -14,6 +14,7 @@
 #include <linux/xattr.h>
 #include <linux/rbtree.h>
 #include <linux/security.h>
+#include <linux/sched.h>
 #include <linux/cred.h>
 #include "overlayfs.h"
 
@@ -299,6 +300,10 @@ static int ovl_dir_read_merged(struct dentry *dentry, struct list_head *list)
 	for (idx = 0; idx != -1; idx = next) {
 		next = ovl_path_next(idx, dentry, &realpath);
 
+		err = ovl_dentry_root_may(dentry, &realpath, MAY_READ);
+		if (err)
+			break;
+
 		if (next != -1) {
 			err = ovl_dir_read(&realpath, &rdd);
 			if (err)
@@ -372,8 +377,13 @@ static int ovl_iterate(struct file *file, struct dir_context *ctx)
 	if (!ctx->pos)
 		ovl_dir_reset(file);
 
-	if (od->is_real)
+	if (od->is_real) {
+		int res = ovl_dentry_root_may(dentry, &(od->realfile->f_path), MAY_READ);
+		if (res)
+			return res;
+
 		return iterate_dir(od->realfile, ctx);
+	}
 
 	if (!od->cache) {
 		struct ovl_dir_cache *cache;