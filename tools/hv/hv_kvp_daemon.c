ec2e20c37d06834413965c91e0eea4da04cfeae4
Seth Forshee <seth.forshee@canonical.com>
2017-05-12 10:56:03 -0500

    Revert "tools: hv: remove unnecessary header files and netlink related code"
    BugLink: http://bugs.launchpad.net/bugs/1679823
    
    This reverts commit e74f56c5d3cc77312f91b910472da4d7bec4c246.
    
    The patches applied for LP #1670544 cause a regression in setting
    MTU for many network drivers, so these commits are being reverted.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

--- tools/hv/hv_kvp_daemon.c
+++ tools/hv/hv_kvp_daemon.c
@@ -22,6 +22,8 @@
  */
 
 
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <sys/poll.h>
 #include <sys/utsname.h>
 #include <stdio.h>
@@ -32,6 +34,7 @@
 #include <errno.h>
 #include <arpa/inet.h>
 #include <linux/hyperv.h>
+#include <linux/netlink.h>
 #include <ifaddrs.h>
 #include <netdb.h>
 #include <syslog.h>
@@ -95,6 +98,10 @@ static struct utsname uts_buf;
 
 #define MAX_FILE_NAME 100
 #define ENTRIES_PER_BLOCK 50
+
+#ifndef SOL_NETLINK
+#define SOL_NETLINK 270
+#endif
 
 struct kvp_record {
 	char key[HV_KVP_EXCHANGE_MAX_KEY_SIZE];
