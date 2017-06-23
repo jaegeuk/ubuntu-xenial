ce6ee1f112012b99d5b9908e2e272084ffdc3717
Stefan Bader <stefan.bader@canonical.com>
2017-06-23 14:50:37 +0200

    Revert "mm: do not collapse stack gap into THP"
    This reverts commit aef97e310da2f5f270b9f8f98832d250d7cea72a to be
    replaced by the upstream patch set.
    
    CVE-2017-1000364
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -587,9 +587,6 @@ int do_huge_pmd_anonymous_page(struct fault_env *fe)
 
 	if (haddr < vma->vm_start || haddr + HPAGE_PMD_SIZE > vma->vm_end)
 		return VM_FAULT_FALLBACK;
-	if (stack_guard_area(vma, haddr) ||
-			stack_guard_area(vma, haddr + HPAGE_PMD_SIZE))
-		return VM_FAULT_FALLBACK;
 	if (unlikely(anon_vma_prepare(vma)))
 		return VM_FAULT_OOM;
 	if (unlikely(khugepaged_enter(vma, vma->vm_flags)))
